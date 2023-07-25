2904be5dc3f902728fee9da44254fe3a80a96f68
emceekoenig <129642888+emceekoenig@users.noreply.github.com>
2023-07-25 12:25:42 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://chelsea-koenig.netlify.app/" target="_blank">
+          Chelsea Koenig
+        </a>{" "}
+        and is{" "}
+        <a href="https://sunny-cactus-8b8fbe.netlify.app/" target="_blank">
+          open-sourced on GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
