b02493424ce4c84f8f17bf8f3920db256b50f644
emceekoenig <129642888+emceekoenig@users.noreply.github.com>
2023-07-26 02:48:35 -0500

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <Weather />
 
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://chelsea-koenig.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Chelsea Koenig
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://sunny-cactus-8b8fbe.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-        .
-      </footer>
+        <footer className="text-center">
+          This project was coded by{" "}
+          <a
+            href="https://chelsea-koenig.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Chelsea Koenig
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://sunny-cactus-8b8fbe.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
