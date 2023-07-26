2c6575ae1016458247a9ca59e844a937ea2713fa
emceekoenig <129642888+emceekoenig@users.noreply.github.com>
2023-07-25 22:28:04 -0500

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,22 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+
       <footer>
         This project was coded by{" "}
-        <a href="https://chelsea-koenig.netlify.app/" target="_blank" rel="noreferrer">
+        <a
+          href="https://chelsea-koenig.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Chelsea Koenig
         </a>{" "}
         and is{" "}
-        <a href="https://sunny-cactus-8b8fbe.netlify.app/" target="_blank" rel="noreferrer">
+        <a
+          href="https://sunny-cactus-8b8fbe.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on GitHub
         </a>
         .
